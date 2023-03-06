19a445850b3d72bd5dcb94f7a697ca3a163198b2
KutsaevPavlo <p1ul@ukr.net>
2023-03-07 00:48:38 +0200

    isonline

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -1,10 +1,11 @@
 import css from "./Friends.module.css";
+import clsx from "clsx";
 
 export const FriendListItem = ({friends}) => {
     return friends.map(friend =>{
             return (
                 <li className={css.item}>
-                <span className={css.status}></span>
+                <span className={clsx(css.status, friend.isOnline && css.isOnline)}></span>
                 <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
                 <p className={css.name}>{friend.name}</p>
               </li>
@@ -12,3 +13,4 @@ export const FriendListItem = ({friends}) => {
         });
 
 }
+
